7b277fa8c278fadd21a020eb774f080b99d0e654
gatotbima1104 <gatotbima2002@gmail.com>
2024-01-24 21:19:01 +0700

    feat: beranda

---
+++ web/app/components/Sidebar.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Image from "next/image";
+import LogoInstagram from "../../src/assets/ig.png";
+import { GoHomeFill } from "react-icons/go";
+import { LuSearch } from "react-icons/lu";
+import { MdOutlineExplore } from "react-icons/md";
+import { BiMoviePlay } from "react-icons/bi";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+import { CiSquarePlus } from "react-icons/ci";
+import { GoHeart } from "react-icons/go";
+import { CgProfile } from "react-icons/cg";
+import { FaThreads } from "react-icons/fa6";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import MenuBtn from "./btn/MenuBtn";
+
+const Sidebar = () => {
+  return (
+    <div className="w-[18.75rem] h-screen justify-center">
+      <div className="items-center flex p-5">
+        <Image
+          src={LogoInstagram}
+          alt="instagram-logo"
+          className="object-fill"
+          width={100}
+        />
+      </div>
+      <div className="flex flex-col p-5 gap-2">
+        <MenuBtn logo={GoHomeFill} text="Home" />
+        <MenuBtn logo={LuSearch} text="Search" />
+        <MenuBtn logo={MdOutlineExplore} text="Explore" />
+        <MenuBtn logo={BiMoviePlay} text="Reels" />
+        <MenuBtn logo={IoPaperPlaneOutline} text="Messages" />
+        <MenuBtn logo={GoHeart} text="Notifications" />
+        <MenuBtn logo={CiSquarePlus} text="Create" />
+        <MenuBtn logo={CgProfile} text="Profile" />
+      </div>
+      {/* place this to the bottom  */}
+      <div className="p-5 mt-24">
+        <MenuBtn logo={FaThreads} text="Threads" />
+        <MenuBtn logo={HiOutlineBars3} text="More" />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- web/app/home/page.tsx
+++ web/app/home/page.tsx
@@ -1,51 +1,63 @@
 import React from "react";
 import Image from "next/image";
 import LogoInstagram from "../src/assets/ig.png";
-import MenuBtn from "../components/MenuBtn";
-import { GoHomeFill } from "react-icons/go";
-import { LuSearch } from "react-icons/lu";
-import { MdOutlineExplore } from "react-icons/md";
-import { BiMoviePlay } from "react-icons/bi";
-import { IoPaperPlaneOutline } from "react-icons/io5";
-import { CiSquarePlus } from "react-icons/ci";
-import { GoHeart } from "react-icons/go";
-import { CgProfile } from "react-icons/cg";
-import { FaThreads } from "react-icons/fa6";
-import { HiOutlineBars3 } from "react-icons/hi2";
+import Profile from "../src/assets/profile.png"
+import Sidebar from "../components/Sidebar";
+
 
 const HomePage = () => {
   return (
-    <div className="w-full h-screen items-center flex">
+    <div className="w-full h-screen flex">
       {/* sidebar components */}
-      <div className="w-[18.75rem] h-screen justify-center">
-        <div className="items-center flex p-5">
-          <Image
-            src={LogoInstagram}
-            alt="instagram-logo"
-            className="object-fill"
-            width={100}
-          />
-        </div>
-        <div className="flex flex-col p-5 gap-2">
-          <MenuBtn logo={GoHomeFill} text="Home" />
-          <MenuBtn logo={LuSearch} text="Search" />
-          <MenuBtn logo={MdOutlineExplore} text="Explore" />
-          <MenuBtn logo={BiMoviePlay} text="Reels" />
-          <MenuBtn logo={IoPaperPlaneOutline} text="Messages" />
-          <MenuBtn logo={GoHeart} text="Notifications" />
-          <MenuBtn logo={CiSquarePlus} text="Create" />
-          <MenuBtn logo={CgProfile} text="Profile" />
-        </div>
-        {/* place this to the bottom  */}
-        <div className="p-5 mt-24">
-          <MenuBtn logo={FaThreads} text="Threads" />
-          <MenuBtn logo={HiOutlineBars3} text="More" />
-        </div>
-      </div>
-
+      <Sidebar/>
+      
       {/* main components */}
-      <div className="border h-screen w-full flex items-center justify-center">
-        <h1>Beranda on Going</h1>
+      <div className="border-l-2 h-screen w-full flex flex-col">
+        <div className="border bg-blue-300 p-5">
+          {/* snap instagram */}
+          <div className="grid grid-cols-12 gap-4">
+            <div className="border flex flex-col items-center gap-2">
+              <button className="rounded-full border bg-blue-50 p-3">
+                <Image
+                  src={Profile}
+                  alt="snap-instagram"
+                  width={50}
+                  height={50}
+                  className="object-contain rounded-full"
+                />
+              </button>
+              <p className="text-xs">gatotbim</p>
+            </div>
+            <div className="border flex flex-col items-center gap-2">
+              <button className="rounded-full border bg-blue-50 p-3">
+                <Image
+                  src={LogoInstagram}
+                  alt="snap-instagram"
+                  width={20}
+                  height={20}
+                  className="object-contain rounded-full"
+                />
+              </button>
+              <p className="text-xs">gatotbim</p>
+            </div>
+            <div className="border flex flex-col items-center gap-2">
+              <button className="rounded-full border bg-blue-50 p-3">
+                <Image
+                  src={LogoInstagram}
+                  alt="snap-instagram"
+                  width={20}
+                  height={20}
+                  className="object-contain rounded-full"
+                />
+              </button>
+              <p className="text-xs">gatotbim</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-red-200">
+          {/* beranda */}
+          <h1>beranda</h1>
+        </div>
       </div>
     </div>
   );
